f54972d8a7ecb4aea23a6d0ab01db712206eebea
loic-roux <69loloro10@gmail.com>
2020-03-15 15:47:37 +0100

    docker

---
+++ after.sh
@@ -0,0 +1,39 @@
+#ssh configure
+echo '
+ChallengeResponseAuthentication no 
+PasswordAuthentication no
+UsePAM no
+PermitRootLogin no
+Port 69' >> /etc/ssh/sshd_config
+
+/etc/init.d/ssh reload
+sudo systemctl reload ssh
+
+# install ansible
+deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main
+
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
+sudo apt update
+sudo apt install ansible
+
+sudo apt update
+sudo apt install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg2 \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   nightly"
+
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io
+
+# after install zsh
+curl https://raw.githubusercontent.com/viasite-ansible/ansible-role-zsh/master/install.sh | bash
