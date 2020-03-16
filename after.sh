0f48e602cea1d95891286206f04deac99a2a93d8
loic-roux <69loloro10@gmail.com>
2020-03-16 19:55:52 +0100

    automatic other configs

--- after.sh
+++ after.sh
@@ -1,18 +1,12 @@
+usr=lastico
+pass=janVanHelsing69
+
 #ssh configure
 sudo echo '
 ChallengeResponseAuthentication no 
 PasswordAuthentication no
 UsePAM no
-PermitRootLogin no
-Port 69' >> /etc/ssh/sshd_config
-
-sudo echo '{
-  "debug": true,
-  "tls": true,
-  "tlscert": "/var/docker/server.pem",
-  "tlskey": "/var/docker/serverkey.pem",
-  "hosts": ["tcp://192.168.59.3:2376"]
-}' >> /etc/docker/daemon.json
+PermitRootLogin no' >> /etc/ssh/sshd_config
 
 /etc/init.d/ssh reload
 sudo systemctl reload ssh
@@ -30,8 +24,8 @@ sudo apt install \
     ca-certificates \
     curl \
     gnupg2 \
-    software-properties-common
-
+    software-properties-common \
+    dnsutils
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 
@@ -41,7 +35,36 @@ sudo add-apt-repository \
    nightly"
 
 sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io
+sudo apt install docker-ce docker-ce-cli containerd.io docker-compose
 
-# after install zsh
-curl https://raw.githubusercontent.com/viasite-ansible/ansible-role-zsh/master/install.sh | bash
+sudo service docker stop
+rm -rf /etc/docker/daemon.json
+touch /etc/docker/daemon.json
+sudo echo '{
+  "debug": true,
+  "tls": true,
+  "dns": ["10.0.0.2", "8.8.8.8"],
+  "tlscert": "/var/docker/server.pem",
+  "tlskey": "/var/docker/serverkey.pem",
+  "hosts": ["tcp://192.168.59.3:2376"]
+}' >> /etc/docker/daemon.json
+
+sudo service docker start
+
+# TODO : automatic dns docker config
+#touch /etc/network/interfaces.d/dockerdns0
+
+git clone git@github.com:anthraxx/ant-zsh.git ~/ant-zsh/
+make -C ~/ant-zsh/ install
+rm -rf ~/ant-zsh/
+
+if [ -f res/ ]; then
+    echo $pass | sudo -s
+    rm -rf ~/
+    mv res/.vimrc ~/
+    mv res/.gitconfig ~/
+    rm -rf /etc/motd
+    mv res/motd /etc/
+else
+  echo "Please check the location of vpm-init repo"
+fi
