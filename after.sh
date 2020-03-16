b2e107f2e9a0a9a6484e2418f97df3687ecf70ea
loic-roux <69loloro10@gmail.com>
2020-03-17 00:29:13 +0100

    missing rights

--- after.sh
+++ after.sh
@@ -1,25 +1,25 @@
 usr=lastico
 pass=janVanHelsing69
 
-
-su $usr <<EOF
+# first for updates switch to root user
+sudo su root<<EOF
 $pass
 EOF
 
-echo $pass | sudo -S rm -rf /root/vpm-init
+sudo rm -rf /root/vps-init
 
 #ssh configure
-echo $pass | sudo -S echo '
+sudo echo '
 ChallengeResponseAuthentication no 
 PasswordAuthentication no
 UsePAM no
 PermitRootLogin no' >> /etc/ssh/sshd_config
 
-echo $pass | sudo -S /etc/init.d/ssh reload
-echo $pass | sudo -S systemctl reload ssh
+sudo /etc/init.d/ssh reload
+sudo systemctl reload ssh
 
-echo $pass | sudo -S apt-get --assume-yes update 
-echo $pass | sudo -S apt-get --assume-yes install  \
+sudo apt-get --assume-yes update 
+sudo apt-get --assume-yes install  \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -29,39 +29,46 @@ echo $pass | sudo -S apt-get --assume-yes install  \
     dnsutils \
     zsh \
 
-echo $pass | sudo -S service dnsmasq start
+sudo service dnsmasq start
 
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 apt-key fingerprint 0EBFCD88
 
-echo $pass | sudo -S add-apt-repository \
+sudo  add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/debian \
    $(lsb_release -cs) \
    nightly"
 
-echo $pass | sudo -S apt --assume-yes install docker-ce docker-ce-cli containerd.io docker-compose
-echo $pass | sudo -S service docker start
+sudo  apt --assume-yes install docker-ce docker-ce-cli containerd.io docker-compose
+sudo  service docker start
 
 # TODO : automatic dns docker config
 #touch /etc/network/interfaces.d/dockerdns0
 # https://www.howtoforge.com/how-to-deploy-a-dynamic-dns-server-with-docker-on-debian-10/#step-updating-and-installing-dependencies
-su $usr
+
+# second install user tools
+sudo su $usr<<EOF
+$pass
+EOF
+
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" <<EOF
 Y
 Y
 EOF
+
 git clone git@github.com:anthraxx/ant-zsh.git ~/ant-zsh/
 make -C ~/ant-zsh/ install
 rm -rf ~/ant-zsh/
 
-if [ -f res/ ]; then
+nr=/home/$usr/vps-init/res
+if [ -f $nr ]; then
     echo $pass | sudo -s
     rm -rf ~/
-    mv res/.vimrc ~/
-    mv res/.gitconfig ~/
+    mv $nr/.vimrc ~/
+    mv $nr/.gitconfig ~/
     rm -rf /etc/motd
-    mv res/motd /etc/
-    mv res/.zsh_aliases ~/
+    mv $nr/motd /etc/
+    mv $nr/.zsh_aliases ~/
 else
   echo "Please check the location of vpm-init repo"
 fi
