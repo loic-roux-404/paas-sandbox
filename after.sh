35019f71449fded223e6ca54f62367ca84825858
loic-roux <69loloro10@gmail.com>
2020-03-17 12:23:50 +0100

    arrange user switch

--- after.sh
+++ after.sh
@@ -1,11 +1,9 @@
 usr=lastico
 pass=janVanHelsing69
 
+sudo su - root
 # first for updates switch to root user
-sudo su root<<EOF
-$pass
-EOF
-
+echo "\n\n$(whoami)\n\n"
 sudo rm -rf /root/vps-init
 
 #ssh configure
@@ -31,23 +29,23 @@ sudo apt-get --assume-yes install  \
 
 sudo service dnsmasq start
 
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 apt-key fingerprint 0EBFCD88
 
-sudo  add-apt-repository \
+add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/debian \
    $(lsb_release -cs) \
    nightly"
 
-sudo  apt --assume-yes install docker-ce docker-ce-cli containerd.io docker-compose
-sudo  service docker start
+apt --assume-yes install docker-ce docker-ce-cli containerd.io docker-compose
+sudo service docker start
 
 # TODO : automatic dns docker config
 #touch /etc/network/interfaces.d/dockerdns0
 # https://www.howtoforge.com/how-to-deploy-a-dynamic-dns-server-with-docker-on-debian-10/#step-updating-and-installing-dependencies
 
 # second install user tools
-sudo su $usr<<EOF
+sudo su - $usr<<EOF
 $pass
 EOF
 
@@ -60,13 +58,10 @@ git clone git@github.com:anthraxx/ant-zsh.git ~/ant-zsh/
 make -C ~/ant-zsh/ install
 rm -rf ~/ant-zsh/
 
-nr=/home/$usr/vps-init/res
+nr=~/vps-init/res
 
-sudo su root<<EOF
-$pass
-EOF
+usermod -s /usr/bin/zsh $usr
 
-rm -rf ~/
 mv $nr/.vimrc ~/
 mv $nr/.gitconfig ~/
 rm -rf /etc/motd
